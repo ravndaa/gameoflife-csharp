16be5d61e40d5288043d264fab81b566002a6467
Stian Ravndal <stian.ravndal@outlook.com>
2020-10-05 00:04:58 +0200

    updated tests, based on review.

--- gameoflife.lib/gameoflifelib.cs
+++ gameoflife.lib/gameoflifelib.cs
@@ -3,6 +3,14 @@ using System.Text.RegularExpressions;
 
 namespace gameoflife.lib
 {
+
+    //  execptions
+    public class IncorrectInputFormat : Exception
+    {
+        public IncorrectInputFormat() { }
+        public IncorrectInputFormat(string message) : base(message) { }
+        public IncorrectInputFormat(string message, Exception inner) : base(message, inner) { }
+    }
     // Game of life library.
     // Using static since we are just providing methods for handling input and create next generation 
     public static class GOL
@@ -10,35 +18,40 @@ namespace gameoflife.lib
         // outputs a object with header, boardsize and board.
         public static GolData ParseInput(string input)
         {
-            
+
             var s = input.Split('\n');
+            // check if we got any input
+            if (s.Length <= 3) throw new IncorrectInputFormat($"Needs 3 or more lines, got: {s.Length}");
             // first line is the Generation line.
             var generation = GetCurrentGenerationNumber(s[0]);
             // second line is the board size first is height and second is width.
             var boardsize = GetCurrentBoardSize(s[1]);
-            
+
             // by the rules we now that the "board" starts at line 3.
-            //var rows = boardsize[0]+2; // rows from input and +2 since board starts there.
-            var board = GetCurrentBoard(s[2..],boardsize[1], boardsize[0]);
+            
+            var board = GetCurrentBoard(s[2..], boardsize[1], boardsize[0]);
 
             var output = new GolData { Generation = generation, Width = boardsize[1], Height = boardsize[0], Board = board };
             return output;
         }
 
-        public static string GenerateNextGeneration(GolData input) {
+        public static string GenerateNextGeneration(GolData input)
+        {
             throw new NotImplementedException("coming soon");
         }
 
         // extract the generation number and return it.
         // throw an error if not correct syntax.
         //
-        private static int GetCurrentGenerationNumber(string input) {
-            var res = input.Replace("Generation ","").Replace(":","");
-            var stoiresult = int.TryParse(res,out int currentGeneration);
-            if(!stoiresult) {
-                throw new Exception("Generation line is wrong format");
+        private static int GetCurrentGenerationNumber(string input)
+        {
+            var res = input.Replace("Generation ", "").Replace(":", "");
+            var stoiresult = int.TryParse(res, out int currentGeneration);
+            if (!stoiresult)
+            {
+                throw new IncorrectInputFormat("Generation line is wrong format");
             }
-            
+
             return currentGeneration;
         }
 
@@ -47,34 +60,41 @@ namespace gameoflife.lib
         // this we know: first char in second line should be an int.
         // third char in second line should be an int
         // 
-        private static int[] GetCurrentBoardSize(string input) {
+        private static int[] GetCurrentBoardSize(string input)
+        {
 
-            var inputaschars = input.ToCharArray();
 
-            var firstcharparsed = int.TryParse(inputaschars[0].ToString(), out int height);
-            if(!firstcharparsed) throw new Exception("Board height not found");
-            
-            var secondcharparsed = int.TryParse(inputaschars[2].ToString(), out int width);
-            if(!secondcharparsed) throw new Exception("Board width not found");
-            
-            return new int[2]{height, width};
+            var h = input.IndexOf(' ');
+            var w = h + 1;
+
+
+            var firstcharparsed = int.TryParse(input[0..h], out int height);
+            if (!firstcharparsed) throw new IncorrectInputFormat("Board height not found");
+
+
+            var secondcharparsed = int.TryParse(input[w..], out int width);
+            if (!secondcharparsed) throw new IncorrectInputFormat("Board width not found");
+
+            return new int[2] { height, width };
         }
 
         // create a jagged char array of current board.
-        private static char[][] GetCurrentBoard(string[] input, int boardwidth, int boardheight) {
-            
+        private static char[][] GetCurrentBoard(string[] input, int boardwidth, int boardheight)
+        {
+            if(input.Length != boardheight) throw new IncorrectInputFormat($"input length = {input.Length} not equal to supplied board height = {boardheight}");
+
             var output = new char[boardheight][];
             for (int i = 0; i < boardheight; i++)
             {
-                
+
                 var row = input[i].Replace("\r", "").Replace("\n", "").ToCharArray();
-                if(row.Length != boardwidth) throw new Exception($"board width for line:{i} is not {boardwidth}");
+                if (row.Length != boardwidth) throw new IncorrectInputFormat($"board width for line:{i} is not {boardwidth}");
                 output[i] = row;
-                
+
             }
-            
+
             return output;
         }
-        
+
     }
 }
--- gameoflife.test/TestGameOfLifeLibrary.cs
+++ gameoflife.test/TestGameOfLifeLibrary.cs
@@ -8,40 +8,129 @@ using gameoflife.lib;
     - https://docs.microsoft.com/en-us/dotnet/core/testing/unit-testing-best-practices
 */
 
-namespace gameoflife.test {
+namespace gameoflife.test
+{
 
-    public class UnitTestGOLLibrary {
-        private readonly string correctGeneration1input = "Generation 1:\n4 8\n........\n....*...\n...**...\n........\n";
-        private readonly string wrongFirstlineGeneration1input = "Generation E:\n4 8\n........\n....*...\n...**...\n........\n";
-        private readonly string wrongSecondlineGeneration1input = "Generation 1:\nE 8\n........\n....*...\n...**...\n........\n";
-        private readonly string wrongLenghtOfColumnsGeneration1input = "Generation 1:\n4 8\n.......\n....*...\n...**...\n........\n"; // one dott is missing.
-        //private readonly string correctGeneration2output = "Generation 2:\n4 8\n........\n...**...\n...**...\n........\n";
-        private static readonly char[][] correctGeneration1Board= new char[][] {
+    public class UnitTestGOLLibrary
+    {
+
+        [Fact]
+        public void TestParseInput_OutputGOLDataStruct()
+        {
+
+            string correctGeneration1input = @"Generation 1:
+4 8
+........
+....*...
+...**...
+........";
+
+            char[][] correctGeneration1Board = new char[][] {
                 new char[] {'.','.','.','.','.','.','.','.'},
                 new char[] {'.','.','.','.','*','.','.','.'},
                 new char[] {'.','.','.','*','*','.','.','.'},
                 new char[] {'.','.','.','.','.','.','.','.'}
-        };
-        private readonly GolData correctParsedGeneration1input = new GolData {Generation=1, Width=8, Height=4, Board=correctGeneration1Board};
+            };
+            GolData correctParsedGeneration1input = new GolData { Generation = 1, Width = 8, Height = 4, Board = correctGeneration1Board };
 
-        [Fact]
-        public void TestParseInput_OutputGOLDataStruct() {
             var parsedGen1output = GOL.ParseInput(correctGeneration1input);
 
-            Assert.Equal(correctParsedGeneration1input.Generation,parsedGen1output.Generation);
-            Assert.Equal(correctParsedGeneration1input.Height,parsedGen1output.Height);
-            Assert.Equal(correctParsedGeneration1input.Width,parsedGen1output.Width);
-            Assert.Equal(correctParsedGeneration1input.Board,parsedGen1output.Board);
+            Assert.Equal(correctParsedGeneration1input.Generation, parsedGen1output.Generation);
+            Assert.Equal(correctParsedGeneration1input.Height, parsedGen1output.Height);
+            Assert.Equal(correctParsedGeneration1input.Width, parsedGen1output.Width);
+            Assert.Equal(correctParsedGeneration1input.Board, parsedGen1output.Board);
         }
 
-        // test if correct error is thrown.
-        // extend to check error message.
         [Fact]
-        public void TestParseInput_WhenInputisWrong() {
-            Assert.Throws<Exception>(() => GOL.ParseInput(wrongFirstlineGeneration1input));
-            Assert.Throws<Exception>(() => GOL.ParseInput(wrongSecondlineGeneration1input));
-            Assert.Throws<Exception>(() => GOL.ParseInput(wrongLenghtOfColumnsGeneration1input));
+        public void TestParseInput_Emptyinput()
+        {
+            var input = "";
+            Assert.Throws<IncorrectInputFormat>(() => GOL.ParseInput(input));
         }
+
+
+
+        [Theory]
+        [ClassData(typeof(CustomTestInputData))]
+        public void TestParseInput_BoardSizes(string input, int height, int width)
+        {
+
+            var parsedGen1output = GOL.ParseInput(input);
+
+            Assert.Equal(height, parsedGen1output.Height);
+            Assert.Equal(width, parsedGen1output.Width);
+        }
+
+        [Fact]
+        public void TestParseInput_WhenInputGenerationIsWrong()
+        {
+            string input = @"Generation E:
+4 8
+........
+....*...
+...**...
+........";
+            var ex = Record.Exception(() => GOL.ParseInput(input));
+            Assert.IsType<IncorrectInputFormat>(ex);
+            Assert.Equal("Generation line is wrong format", ex.Message);
+
+        }
+
+        [Fact]
+        public void TestParseInput_WhenInputBoardWidthIsWrong()
+        {
+            string input = @"Generation 1:
+5 E
+........
+....*...
+...**...
+........";
+            var ex = Record.Exception(() => GOL.ParseInput(input));
+            Assert.IsType<IncorrectInputFormat>(ex);
+            Assert.Equal("Board width not found", ex.Message);
+
+        }
+
+        [Fact]
+        public void TestParseInput_WhenInputBoardHeightIsWrong()
+        {
+            string input = @"Generation 1:
+E 6
+........
+....*...
+...**...
+........";
+            var ex = Record.Exception(() => GOL.ParseInput(input));
+            Assert.IsType<IncorrectInputFormat>(ex);
+            Assert.Equal("Board height not found", ex.Message);
+
+        }
+
+        [Fact]
+        public void TestParseInput_WhenInputBoardDoesNotMatchHeightIsWrong() {
+            string input = @"Generation 1:
+4 8
+........
+....*...
+...**...";
+            var ex = Record.Exception(() => GOL.ParseInput(input));
+            Assert.IsType<IncorrectInputFormat>(ex);
+            Assert.Equal("input length = 3 not equal to supplied board height = 4", ex.Message);
+        }
+
+        [Fact]
+        public void TestParseInput_WhenInputBoardDoesNotMatchWidthIsWrong() {
+            string input = @"Generation 1:
+4 9
+........
+....*...
+...**...
+........";
+            var ex = Record.Exception(() => GOL.ParseInput(input));
+            Assert.IsType<IncorrectInputFormat>(ex);
+            Assert.Equal("board width for line:0 is not 9", ex.Message);
+        }
+
 
     }
 }
